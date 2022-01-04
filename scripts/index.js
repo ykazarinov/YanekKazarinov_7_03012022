55e8bd6800df57fe52aff65cfe6b161531c70426
ykazarinov <kazarinov.yanek@gmail.com>
2022-01-04 15:16:07 +0100

    cards styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,13 @@
 class init{
-    // async getCards() {
-    //     const cards = await fetch('data/recipes.js')
-    //     .then(response => response.json())
-    //     console.log(cards)
-    //     return cards
-    // }
-
     displayData(cards) {
         const cardsSection = document.querySelector("#cards-section")
         cards.forEach((card) => {
             const cardModel = new cardTemplate(card)
             const cardDOM = cardModel.getCardDOM()
-            
             cardsSection.appendChild(cardDOM)
-
-                       
         })
     }
 }
 
 let myInit = new init()
-// let allData = myInit.getCards()
 myInit.displayData(recipes)
--- scripts/templates/card.js
+++ scripts/templates/card.js
@@ -8,16 +8,59 @@ class cardTemplate{
         this.ustensils = data.ustensils
     }
 
+    getIngredients(){
+       
+        let content = `<dl>`
+        this.ingredients.forEach(myIngredient => {
+            let ingredient = myIngredient.ingredient
+            let quantity
+            if(myIngredient.quantity){
+                ingredient += ': '
+                quantity = myIngredient.quantity
+            }else{
+                quantity = ''
+            }
+            let unit
+            if(myIngredient.unit){
+                if(myIngredient.unit === 'grammes'){
+                    unit = 'g'
+                }else{
+                    unit = myIngredient.unit
+                }
+                
+            }else{
+                unit = ''
+            }
+            let ingredDOM =
+            `<dt class='font-weight-bold'>${ingredient}</dt>
+            <dd>${quantity} ${unit}</dd>
+            `
+            content += ingredDOM
+            
+        })
+        content += `</dl>`
+        return content
+    }
+
     getCardDOM(){
         const card = document.createElement( 'div' )
-        card.classList.add('col-4')
+        card.classList.add('col-12')
+        card.classList.add('col-md-6')
+        card.classList.add('col-lg-4')
+        card.classList.add('mb-3')
+        card.classList.add('card-container')
         let cardContent = 
-        `<div class="card">
-            <img class="card-img-top" src="images/empty.jpg" alt="Card image cap">
-            <div class="card-body">
-            <h5 class="card-title">${this.name}</h5>
-            <div class='time'>${this.time} min</div>
-            <p class="card-text">${this.desc}</p>
+        `<div class="card bg-light border-0">
+            <img class="card-img-top" src="images/empty.jpg" alt="${this.name}">
+            <div class="card-body flex">
+                <div class = 'row title mb-2'>
+                    <h5 class="card-title" title='${this.name}'>${this.name}</h5>
+                    <div class='time'>${this.time} min</div>
+                </div>    
+                <div class='row desk'>
+                    <div class='col-6 ingredients'>${this.getIngredients()}</div>
+                    <p class="col-6 card-text">${this.desc}</p>
+                </div>
             
             </div>
         </div>`
