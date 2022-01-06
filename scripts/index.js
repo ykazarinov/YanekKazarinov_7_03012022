15089b87d7ec1a7c277103af1551c996d8454773
ykazarinov <kazarinov.yanek@gmail.com>
2022-01-06 11:54:15 +0100

    searchInput (name, ingredients, deskription etc) for cards

--- scripts/index.js
+++ scripts/index.js
@@ -29,19 +29,69 @@ class init{
         let sortedCards = []
         let that = this
         searchInput.addEventListener('input', function(){
+            
+            // hide the message with null results
+            if(!document.querySelector('#aucune-recette').classList.contains('hidden')){
+                document.querySelector('#aucune-recette').classList.add('hidden')
+            }
+
             if(searchInput.value.length >= 3){
                 
                 that.removeCards()
                 sortedCards = []
 
                 for(let i = 0; i < recipes.length; i++){
-                    if(recipes[i].name.indexOf(searchInput.value) !== -1){
+                    let isAnyOption = false
+
+                    // name
+                    if( recipes[i].name.toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1){
                         sortedCards.push(recipes[i]) 
+                        isAnyOption = true
+                        
                     }
+                    // appliance
+                    if(isAnyOption === false){
+                        if(recipes[i].appliance.toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1){
+                            sortedCards.push(recipes[i])
+                            isAnyOption = true
+                        }
+                    }
+
+                    // ingredients
+                    if(isAnyOption === false){
+                        for(let j = 0; j < recipes[i].ingredients.length; j++){
+                            if(recipes[i].ingredients[j].ingredient.toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1){
+                                sortedCards.push(recipes[i])
+                                isAnyOption = true
+                            }
+                        }
+                    }
+
+                    // ustensils
+                    if(isAnyOption === false){ 
+                        for(let j = 0; j < recipes[i].ustensils.length; j++){
+                            if(recipes[i].ustensils[j].toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1){
+                                sortedCards.push(recipes[i])
+                                isAnyOption = true
+                            }
+                        }
+                    }
+
+                    // description
+                    if(isAnyOption === false){
+                        if(recipes[i].description.toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1){
+                            sortedCards.push(recipes[i])
+                            isAnyOption = true
+                        }
+                    }
+                    // null results
+                    if(isAnyOption === false){
+                         document.querySelector('#aucune-recette').classList.remove('hidden')
+                    }
+
                 }
 
                 that.displayData(sortedCards)
-
 
             }
             else{
